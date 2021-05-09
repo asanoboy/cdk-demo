8d9e27d92600de4ed2f1ee6143e1b11bfe2a1931
Yosuke Asano <yosuke.asano@flagellin.com>
2021-05-09 12:05:20 +0900

    Add Vpc

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -1,9 +1,11 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { Vpc } from "@aws-cdk/aws-ec2";
 
 export class AppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new Vpc(this, "AppVpc");
   }
 }
